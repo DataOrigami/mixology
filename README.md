0cf2180beaf8f608252f93e674bae7099960d3fb
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2013-08-20 22:44:06 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-mixology
+Mixology
 ========
 
-“Beer's intellectual. What a shame so many idiots drink it.” 
+> “Beer's intellectual. What a shame so many idiots drink it.” 
+
+###Every proper club needs a proper club cocktail.
+Can we algorithmically find ours using a database of cocktail drinks?
+
+Here are our goals to accomplish:
+
+- Determine the ingredients (and perhaps the directions to make the cocktail, too)
+  - This may require an objective function. 
+  - Using the database to weed out **bad** combinations is good too!
+  - Should have some flair to it.
+  - More data is also better. We should look at popularity too.
+- Determine a name for the cocktail (this should be data determined too)
+  - Naively, gather all names of cocktails, split on whitespace, and randomly pick 
+    randomly.
+  - Better: considering the ingredients we have chosen, look for a pair or triplet
+   of existing cocktails that can be "added" to produce our cocktail.
+  - using Google Trends to find "hot names", i.e. "One Chevron Direction" would be hilarious. 
+- Look for interesting relationships in the dataset. "orange juice => vodka"?
+- Create a cool postcard-sized graphic about our cocktail (with recipe). We'll use this to create actual postcards to promote our club: "Dear P.M. Harper, I've noticed you could use a drink..."
+- Can we get a local drinking establishment to serve it? Maybe exclusively to our members/meetings?
+- Actually drink it. Our next meeting will be supplied with our new Data Origami cocktail. 
--- utils.py
+++ utils.py
@@ -1,44 +1,175 @@
 from requests import get
 from bs4 import BeautifulSoup
-
+import numpy as np
+from fractions import Fraction
+import pandas as pd
+from time import sleep
 
 def find_measures(bsoup, id):
   measures = bsoup.find_all('div', class_="recipeMeasure")
-  return [ parse_measures(m, id) for m in measures ]
+  return list( set( filter(None, [ parse_measures(m, id)for m in measures ]) ) )
 
 def find_directions(bsoup, id):
   directions = bsoup.find_all('div', class_="recipeDirection")
-  return filter(None, [ parse_directions(m, id) for m in directions ])
+  return list( set( filter(None, [ parse_directions(m, id) for m in directions ]) ) )
 
-
-
-def parse_measures(measure_info,id ):
+def parse_measures(measure_info, id ):
+  """return (id, volume, ingredient, 'measure')"""
   contents = measure_info.contents
-  return ( id, contents[0], contents[1].text )
+  if "Flame" in contents[0]:
+    return None 
+  elif "Place" in contents[0]:
+    return ( id, "1", "pieces of " + contents[1].text, "measure"  )
+  elif "Fill" in contents[0]:
+    return ( id, "1", "pieces of " + contents[1].text, "measure"  )
+  return ( id, contents[0], contents[1].text, "measure"  )
 
 def parse_directions(direction_info, id):
   contents = direction_info.contents
   if contents[0] in ("Build", "Serve in a ", "Muddle/shake"):
     return None
   elif contents[0] == "Fill with":
-    return "Ice"
-  elif contents[0] == "Top with":
-    return contents[1].text
+    return (id,1, "Fill with ice", "direction")
+  elif contents[0] == "Top with ": #need to investigate this one further.
+    return (id, 1, "Top with "+ contents[1].text , "direction")
   elif contents[0] == "Shake in ":
-    return "Shake in iced cocktail shaker & strain"
+    return (id, 1, "Shake in iced cocktail shaker & strain" , "direction")
   elif contents[0] == "Stir in ":
-    return "Stir in mixing glass with ice & strain" 
+    return (id, 1, "Stir in mixing glass with ice & strain" , "direction")
   elif contents[0] == "Add ":
-    return contents[1].text
+    return (id, 1, contents[1].text, "direction")
   else:
     return None
 
+def parse_volumes(measures):
+  """This takes in measures, and replaces the volumes with floats"""
+  return [ (m[0], ounce_to_float(m[1]), m[2].lower(), m[3]) for m in measures ]
+
+def ounce_to_float(oz_string):
+  """converts something like '1 1/2 oz ' to 1.5"""
+  oz_string = oz_string.strip()
+  oz_split = oz_string.split(" ")
+  if "dash" in oz_split[-1]:
+    return 0.1 #?? what the hell is a dash
+  else:
+    s=0.0
+    i=0
+    while 1:
+      try:
+        s += Fraction(oz_split[i])
+        i+=1
+        if i%10==0:
+          print "Broke! %d"%i
+      except ValueError:
+        return s
+      except IndexError:
+        return s
+
+def get_name(bs,id):
+  #todo
+  pass
+
+def measures_to_df(all_measures):
+  df = pd.DataFrame.from_records( all_measures,
+                                  columns=['id', 'volume', 'ingredient', 'measure']
+                                  )
+  return df
+  #return q
+
+def df_to_pivot(df):
+  return pd.pivot_table(df, values = "volume", rows="id", cols= "ingredient", aggfunc=np.sum, fill_value = 0).astype(float)
+
+
+def merge_columns(df):
+    same_columns = [ 
+       ['gold rum', 'golden rum'],
+       ['yolk of egg', 'yolk of fresh egg', 'egg yolk'],
+       [ 'canadian club', 'canadian club whisky', 'canadian whiskey', 'canadian whisky'],
+       ['egg white', 'white of an egg'],
+       ['worcester sauce', 'worcestershire', 'worcestershire sauce'],
+       ['johnnie walker', 'johnny walker'],
+       ['creme de vanilla', 'creme de vanille'],
+       ['passion fruit juice', 'passion fruit nectar', 'passion fruit syrup'],
+       ['vanilla', 'vanilla extract'],
+       ['mint sprig', 'mint sprigs', 'sprigs of mint'],
+       ['lime (or lemon) juice', 'lime juice', 'lemon (or lime) juice', 'lemon juice'],
+       ['orgeat', 'orgeat syrup'],
+       [ 'calisay', 'calisaya'],
+       [ 'clove', 'cloves'],
+       ['frais','fraise'],
+       ['ice','iced'],
+       ['jamaica ginger', 'jamaican ginger', 'jamaican ginger extract'],
+       ['kirsch','kirschwasser'],
+       ['lillet','lillet blanc'],
+       ['shaved ice', 'pieces of shaved ice'],
+       ['dry vermouth', 'vermouth, dry'],
+       ['vieille cure', 'vielle cure'],
+       ['egg', 'eggs','whole egg', 'whole eggs'],
+       ['blackberry brandy', 'blackberry flavored brandy'],
+       ['mandarette', 'mandarinette'],
+       ["myers's", "myers's rum"],
+       ['port', 'port wine'],
+       ['gomme', 'gomme syrup'],
+       ['apricot brandy','apricot flavored brandy'],
+       ["pimm's", "pimm's cup"],
+       ["rose's", "rose's lime juice"],
+       ['sambuca', 'sambucca'],
+       ['sauterne', 'sauterne wine'],
+       ['scotch', 'scotch whisky'],
+       ['tabasco', 'tabasco sauce'],
+       ['angostura', 'angostura bitters'],
+       ['mint leaf', 'mint leaves'],
+       ['limes', 'lime'],
+       ['geneva gin', 'genever gin'],
+       ['jamaica rum', 'jamaican rum'],
+       ['prunella', 'prunelle', 'prunelle liqueur'],
+       ["creme d'yvette",'creme de yvette','creme yvette'],
+       ['creme de noyau', 'creme de noyeau', 'creme de noyeaux'],
+       ['creme de banana', 'creme de banane'],
+       ['ginger brandy', 'ginger flavored brandy'],
+       ['orange flavored gin', 'orange gin'],
+       ['catsup', 'tomato catsup'],
+       ['bourbon', 'bourbon whisky'],
+       ['campari', 'campari bitters'],
+       ['caloric', 'caloric punch'],
+
+
+
+    
+    ]
+    df_ = df.copy()
+    for columns in same_columns:
+      c_keep = columns[0]
+      for c in columns[1:]:
+         try:
+            df_[c_keep] += df_[c]
+            del df_[c]
+         except Exception as e: 
+             print e 
+    return df_ 
 
 if __name__=="__main__":
-  drink_id = 3819
-  URL = "http://www.cocktaildb.com/recipe_detail?id=%d"%drink_id
-  bs = BeautifulSoup(get(URL).text)
-  print find_directions(bs,drink_id)
-  print find_measures(bs,drink_id)
-
+  upper_bound = 1
+  lower_bound = 4600
+  drink_ids = range(lower_bound,upper_bound)
+  recipes = {}
+  for drink_id in drink_ids:
+      URL = "http://www.cocktaildb.com/recipe_detail?id=%d"%drink_id
+      try:
+        bs = BeautifulSoup(get(URL).text)
+      except ConnectionError as e:
+        print e 
+        sleep(10)
+        bs = BeautifulSoup(get(URL).text)      
+      recipes[drink_id] = {
+              "measures":find_measures(bs,drink_id),
+              "directions": find_directions(bs,drink_id)
+      } 
+      print drink_id 
+  all_measures = sum( [ v["measures"] for k,v in recipes.iteritems()], [] )
+  all_directions = [v["directions"] for k,v in recipes.iteritems()]
+  all_measures = parse_volumes( all_measures )
+  data = measures_to_df( all_measures )
+  data.to_csv( "drinks%d-%d.csv"%(lower_bound,upper_bound) )
+  print "done"
 
