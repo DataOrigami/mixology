c9396c99a58bcf779551e1d0163e550a12368f97
Paul Inder <paulinder@gmail.com>
2013-09-17 19:48:19 -0400

    added docstring

--- buildDataset.py
+++ buildDataset.py
@@ -1,7 +1,11 @@
+"""
+buildDataset
+
+Implements a function ``buildListOfDict`` to parse the drink recipe dataset 
+(./datasets/6K.csv) and turn it into a list of drink recipe dictionaries.
 
 
-
-
+"""
 
 
 def buildListOfDict():
@@ -18,9 +22,15 @@ def buildListOfDict():
         drink['ingredients'] = []
         for i in xrange(len(recipe)):
             drink['ingredients'].append(recipe[i])
+            #quantity, ing = parseIngredient(recipe[i])
+            #drink['ingredients'].append({'quantity':quantity, 'type':ing})
         dataset.append(drink)
     return dataset
 
+def parseIngredient(ing):
+    #   was going to split each ingredient string into 'quantity' and 'type'..
+    #   e.g. quantity = 2 oz, type = Vodka
+    pass
 
 if __name__=='__main__':
     drinklist = buildListOfDict()
